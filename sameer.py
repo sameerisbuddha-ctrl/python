93372cdd8d2619dc4e671848805de7c604b15859
Sameer Sagar <sameerisbuddha@gmail.com>
2026-04-20 15:51:47 +0530

    Initial commit

---
+++ sameer.py
@@ -0,0 +1,15 @@
+a=7
+b=8
+#print (a * 3 + b);
+#print (type (a))
+
+#print(a.upper().isupper())
+#print(a)
+#print(len(a))
+#print(a[2])
+#print(a.index("eer"))
+#print(a.replace("S","Z"))
+
+print((a+b)%3)
+print(str(a))
+print(type(a))
